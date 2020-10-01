80c7f15fa4afbf0433e1044cf71ed5aef0d06218
KeenanB <k.bratley626@gmail.com>
2020-10-01 12:15:42 +0100

    trying to get database to not exit after docker-compose up
    Signed-off-by: KeenanB <k.bratley626@gmail.com>

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,7 @@
-#! /bin/sh
-until nc -z mysql 3306; do
-echo "waiting on db"
-sleep 2
-done
-sleep 2
-python3 app.py
+# #! /bin/sh
+# until nc -z mysql 3306; do
+# echo "waiting on db"
+# sleep 2
+# done
+# sleep 2
+# python3 app.py
