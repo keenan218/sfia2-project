15d5bf4488a958d7fab66e845a9934d03263f6e1
KeenanB <k.bratley626@gmail.com>
2020-10-09 16:33:26 +0100

    lanch rds and ec2 instance from tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-    version = "~> 2.0"
-    region = "eu-west-2"
-    shared_credentials_file = "~/.aws/credentials"
+  version                 = "~> 2.0"
+  region                  = "eu-west-2"
+  shared_credentials_file = "~/.aws/credentials"
 }
 
 resource "aws_instance" "EC2" {
@@ -10,3 +10,17 @@ resource "aws_instance" "EC2" {
   key_name      = var.pem-key
 }
 
+resource "aws_db_instance" "RDS" {
+  identifier           = "test-db"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "test_db"
+  username             = "root"
+  password             = "password"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  apply_immediately    = true
+}
