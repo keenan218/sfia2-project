98308eedb313b2d1632ff63e1e9f9f874a4ff742
KeenanB <k.bratley626@gmail.com>
2020-10-11 22:53:01 +0100

    removed changes - 'drop database before creation to remove error'
    Signed-off-by: KeenanB <k.bratley626@gmail.com>

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -4,7 +4,7 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = str(getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_DATABASE_URI'] = str(getenv('TEST_DATABASE_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = str(getenv('SECRET_KEY'))
 db = SQLAlchemy(app)
